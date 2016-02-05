675e265d3665db53f5a1fa9f87171c6adfb2fd3d
dodik gaghan <dodik@montazze.com>
2016-02-05 14:31:41 +0700

    add body parser

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  * @Author: Dodik Gaghan
  * @Date:   2016-02-04 16:05:33
  * @Last Modified by:   Dodik Gaghan
- * @Last Modified time: 2016-02-05 09:50:26
+ * @Last Modified time: 2016-02-05 14:31:07
  */
 
 'use strict';
@@ -11,6 +11,7 @@ var express = require('express');
 var http = require('http');
 var ECT = require('ect');
 var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 var config = require('./config');
 var app = exports.app = express();
 var server = http.createServer(app);
@@ -26,6 +27,10 @@ app.set('view engine', 'html');
 app.engine('html', ectRenderer.render);
 app.set('views', config.root + '/app/views');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(express.static(config.root + '/public'));
 app.use(methodOverride());
 
@@ -35,7 +40,7 @@ require('deployd').attach(server, {
 });
 console.log('Using MongoDB:', config.db);
 
-app.dpd = require('dpd-js-sdk')('http://localhost:3000', '/' );
+app.dpd = require('dpd-js-sdk')('http://localhost:3000', '/');
 app.controllers = require(config.root + '/app/controllers')(app);
 require(config.root + '/app/routes.js')(app);
 
